a41957edccdc0532d4ba1f2a43034792a9a79930
ytHu <>
2023-07-10 14:26:54 +0000

    add hand made AC network

---
+++ isaacgymenvs/learning/handwrite_player.py
@@ -0,0 +1,20 @@
+import copy
+from datetime import datetime
+from gym import spaces
+import numpy as np
+import os
+import time
+import yaml
+
+from rl_games.algos_torch import a2c_continuous
+from rl_games.algos_torch import torch_ext
+from rl_games.algos_torch import central_value
+from rl_games.algos_torch.running_mean_std import RunningMeanStd
+from rl_games.common import a2c_common
+from rl_games.common import datasets
+from rl_games.common import schedulers
+from rl_games.common import vecenv
+
+import torch
+from torch import optim
+
